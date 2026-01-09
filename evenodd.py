57022397a94b35829826361bef04ced484621e1c
Japhet <japhetsesidoe@gmail.com>
2026-01-09 16:32:54 +0000

    a file to determine if numbers are even or odd

---
+++ evenodd.py
@@ -0,0 +1,9 @@
+print("this file allows you to check for even or odd numbers")
+
+age = int(input("what is your age: "))
+ 
+if age % 2 == 0:
+    print("the age is even")
+else:
+    print("the age is not even")
+
---
+++ test.py
@@ -0,0 +1,17 @@
+print("helloworld")
+a = 5
+b = 10
+sum = a+b
+print(sum)
+product = a*b
+print(product)
+divide = a/b
+print(divide)
+subtract = a-b
+print(subtract)
+print("my name is japhet")
+print("im going to study IT")
+print("55")
+number = 55
+print(number)
+print("5" + "5")
