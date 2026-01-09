0ef942383c76f278e805c6c16a7fb15d43422281
Japhet <japhetsesidoe@gmail.com>
2026-01-09 17:20:15 +0000

    this file is used to compare 3 numbers

---
+++ user.py
@@ -0,0 +1,10 @@
+number1 = int(input("enter the first number: "))
+number2 = int(input("enter the second number: "))
+number3 = int(input("enter the third number: "))
+
+if number1>=number2 and number1>=number:
+    print("the larger number is number1: " , number1)
+elif number2>=number1 and number2>=number3:
+    print("the larger number is number2: " , number2)
+else:
+    print("the larger number is number3: " , number3)    
